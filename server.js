626b3f183ffddfc7c97b0538893765c46a5d28c8
Gabriel Pessoa <gabrielpessoaplay@gmail.com>
2026-09-25 19:12:51 -0300

    Personaliza convite com foto e animacoes

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const path = require('path');
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
